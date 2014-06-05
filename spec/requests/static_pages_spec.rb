4262d096ba6e35b48b0db651db6facc8a063fddf
Jayson <jaysonwcs@gmail.com>
2014-06-05 00:26:33 -0300

    static-pages (with Spork)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,37 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Salas de Cinema')
     end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Salas De Cinema | Home")
+    end
   end
+  
+  describe "Help page" do
+
+    it "should have the content 'Ajuda'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Ajuda')
+    end
+    
+    it "should have the title 'Ajuda'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Salas De Cinema | Ajuda")
+    end
+  end
+  
+  describe "About page" do
+
+    it "should have the content 'Sobre'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('Sobre')
+    end
+    
+    it "should have the title 'Sobre'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Salas De Cinema | Sobre")
+    end
+  end
+
 end
