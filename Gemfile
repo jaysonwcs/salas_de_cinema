e12efcdb23cd6db0e0c83f22dfe5060910a40ad0
Jayson W. C. Silva <jaysonwcs@gmail.com>
2014-06-13 22:52:55 -0300

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -4,26 +4,43 @@ ruby '1.9.3'
 
 gem 'rails', '4.0.4'
 
+gem 'bootstrap-sass', '2.3.2.0'
+#Framework for CSS
+gem 'sprockets', '2.11.0'
+#Asset organizer
+
 group :development, :test do
   gem 'sqlite3', '1.3.8'
+  #Test Database
   gem 'rspec-rails', '2.13.1'
+  #Rspec (tests with Ruby)
   gem 'guard-rspec', '2.5.0'
+  #Monitor (if sth changes, it will start the Rspecs tests)
   gem 'listen', '2.7.5'
+  #Dependency for Guard
   gem 'spork-rails', '4.0.0'
+  #Pre-Load Rspec
   gem 'guard-spork', '1.5.0'
+  
   gem 'childprocess', '0.3.6'
+  
   gem 'debugger'
+  #Terminal debugger
   gem 'ruby-debug-ide', '0.4.23.beta1'
+  #IDE debugger (for Aptana)
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  #Uses Rspec for web pages
 end
 
 gem 'sass-rails', '4.0.1'
+#CSS Preprocessor
 gem 'uglifier', '2.1.1'
 gem 'coffee-rails', '4.0.1'
+#Javascript compiler (Coffeescript)
 gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
@@ -34,5 +51,6 @@ end
 
 group :production do
   gem 'pg', '0.15.1'
+  #Postgree SQL
   gem 'rails_12factor', '0.0.2'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,51 +1,63 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page }
   
-  let(:base_title) {"Salas de Cinema"}
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
-
-    it "should have the content 'Salas de Cinema'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Salas de Cinema')
-    end
+    before { visit root_path }
     
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title(base_title)
-    end
+    let(:heading)    { 'Salas de Cinema' }
+    let(:page_title) { '' }
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
-  
+
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Ajuda'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Ajuda')
-    end
-    
-    it "should have the title 'Ajuda'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Ajuda")
-    end
+    let(:heading)    { 'Ajuda' }
+    let(:page_title) { 'Ajuda' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    let(:heading)    { 'Sobre' }
+    let(:page_title) { 'Sobre' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    let(:heading)    { 'Contato' }
+    let(:page_title) { 'Contato' }
+
+    it_should_behave_like "all static pages"
   end
   
-  describe "About page" do
-
-    it "should have the content 'Sobre'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('Sobre')
-    end
-    
-    it "should have the title 'Sobre'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | Sobre")
-    end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Sobre"
+    expect(page).to have_title(full_title('Sobre'))
+    click_link "Ajuda"
+    expect(page).to have_title(full_title('Ajuda'))
+    click_link "Contato"
+    expect(page).to have_title(full_title('Contato'))
+    click_link "Home"
+    click_link "Cadastre-se!"
+    expect(page).to have_title(full_title('Cadastro'))
+    click_link "salas de cinema"
+    expect(page).to have_title(full_title(''))
   end
-
 end
